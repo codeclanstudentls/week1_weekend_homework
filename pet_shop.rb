7e0a75bc92035089c2ad9c93b5edf47919db4536
Logan Smith <hamspamandeggs@gmail.com>
2016-11-04 13:41:51 +0000

    added to Answer2

--- pet_shop.rb
+++ pet_shop.rb
@@ -3,9 +3,9 @@ def total_cash(admin_total)
   return admin_total
 end
 
-def add_or_remove_cash__add(cash_in, total_cash)
+def add_or_remove_cash(cash_in, total_cash)
   #expecting to ad 10 to admin total i.e. 1000 + 10 but also the value being added?
-  cash = @pet_shop[:admin][:total_cash] + 10
-
-
+  cash_in = 10
+  total_cash = @pet_shop[:admin][:total_cash] + cash_in
+  return total_cash, cash_in
 end
